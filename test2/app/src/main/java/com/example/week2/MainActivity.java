581fe5d911230ae5e3136e9b2c00a1decbc6a0b3
U200709046 <meleksayan11@gmail.com>
2023-10-18 12:41:25 +0300

    week2

---
+++ test2/app/src/main/java/com/example/week2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnArrayAdapter;
+    Button btnCustomAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        btnArrayAdapter= findViewById(R.id.ButtonArrayAdapter);
+        btnArrayAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ArrayAdapterActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        btnCustomAdapter = findViewById(R.id.ButtonCustemAdapter);
+        btnCustomAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CustomAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ test2/app/src/main/java/com/example/week2/listview_row.java
@@ -0,0 +1,14 @@
+package com.example.week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listview_row extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview_row);
+    }
+}
